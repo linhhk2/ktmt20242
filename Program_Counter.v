c1884704249e67a6b2ae2c99b25e8c4160a12ea1
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-06-27 22:15:04 +0700

    Update Program_Counter.v

--- Program_Counter.v
+++ Program_Counter.v
@@ -1,11 +1,12 @@
-// Program_Counter.v
+// --- File: Program_Counter.v ---
 module Program_Counter(
-    input clk, reset,
+    input clk,
+    input rst_n,
     input  [31:0] pc_in,
     output reg [31:0] pc_out
 );
-    always @(posedge clk or posedge reset) begin
-        if (reset)
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
             pc_out <= 32'h00000000;
         else
             pc_out <= pc_in;
