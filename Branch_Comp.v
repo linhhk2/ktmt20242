cae5d6f5fe73cdb162f4c5f0339a099663cffb0c
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-06-27 22:17:18 +0700

    Update Branch_Comp.v

--- Branch_Comp.v
+++ Branch_Comp.v
@@ -1,26 +1,21 @@
-// Branch_Comp.v
+// --- File: Branch_Comp.v ---
 `include "defines.v"
 
 module Branch_Comp(
-    input [31:0] A, B,
+    input [31:0] A,
+    input [31:0] B,
+    input        BrUn,
     input [2:0]  funct3,
     output reg   BranchTaken
 );
-    wire eq    = (A == B);
-    wire neq   = (A != B);
-    wire lt_s  = ($signed(A) < $signed(B));
-    wire gte_s = ($signed(A) >= $signed(B));
-    wire lt_u  = (A < B);
-    wire gte_u = (A >= B);
-
     always @(*) begin
         case (funct3)
-            `FUNCT3_BEQ:  BranchTaken = eq;
-            `FUNCT3_BNE:  BranchTaken = neq;
-            `FUNCT3_BLT:  BranchTaken = lt_s;
-            `FUNCT3_BGE:  BranchTaken = gte_s;
-            `FUNCT3_BLTU: BranchTaken = lt_u;
-            `FUNCT3_BGEU: BranchTaken = gte_u;
+            `FUNCT3_BEQ:  BranchTaken = (A == B);
+            `FUNCT3_BNE:  BranchTaken = (A != B);
+            `FUNCT3_BLT:  BranchTaken = ($signed(A) < $signed(B));
+            `FUNCT3_BGE:  BranchTaken = ($signed(A) >= $signed(B));
+            `FUNCT3_BLTU: BranchTaken = (A < B);
+            `FUNCT3_BGEU: BranchTaken = (A >= B);
             default:      BranchTaken = 1'b0;
         endcase
     end
