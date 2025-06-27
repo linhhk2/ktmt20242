efddc4004872a4b5862709c6d757c9daaa1a6e53
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-06-27 22:16:32 +0700

    Update ALU_decoder.v

--- ALU_decoder.v
+++ ALU_decoder.v
@@ -1,6 +1,4 @@
-// ALU_decoder.v
-// Tạo tín hiệu 4-bit cụ thể cho ALU.
-
+// --- File: ALU_decoder.v ---
 `include "defines.v"
 
 module ALU_decoder(
@@ -11,8 +9,7 @@ module ALU_decoder(
 );
     always @(*) begin
         case (ALUOp)
-            // Dành cho các lệnh R-Type
-            2'b00:
+            2'b00: // R-Type
                 case (funct3)
                     `FUNCT3_ADD_SUB: alu_control = (funct7 == `FUNCT7_SUB) ? `ALU_SUB : `ALU_ADD;
                     `FUNCT3_SLL:     alu_control = `ALU_SLL;
@@ -24,13 +21,9 @@ module ALU_decoder(
                     `FUNCT3_AND:     alu_control = `ALU_AND;
                     default:         alu_control = 4'bxxxx;
                 endcase
-
-            // Dành cho lw, sw, auipc, jal, jalr
-            2'b01:
+            2'b01: // For lw, sw, auipc, jal, jalr
                 alu_control = `ALU_ADD;
-
-            // Dành cho các lệnh I-Type (số học)
-            2'b10:
+            2'b10: // I-Type Arithmetic
                 case (funct3)
                     `FUNCT3_ADDI:       alu_control = `ALU_ADD;
                     `FUNCT3_SLTI:      alu_control = `ALU_SLT;
@@ -39,15 +32,11 @@ module ALU_decoder(
                     `FUNCT3_ORI:       alu_control = `ALU_OR;
                     `FUNCT3_ANDI:      alu_control = `ALU_AND;
                     `FUNCT3_SLLI:      alu_control = `ALU_SLL;
-                    // Dòng 35 của bạn bây giờ sẽ khớp với defines.v
-                    `FUNCT3_SRLI_SRAI: alu_control = (funct7 == `FUNCT7_SRA) ? `ALU_SRA : `ALU_SRL; // SRAI cần kiểm tra funct7
+                    `FUNCT3_SRLI_SRAI: alu_control = (funct7 == `FUNCT7_SRA) ? `ALU_SRA : `ALU_SRL;
                     default:           alu_control = 4'bxxxx;
                 endcase
-
-            // Dành cho các lệnh Branch
-            2'b11:
-                alu_control = 4'bxxxx; // Không dùng
-
+            2'b11: // Branch
+                alu_control = 4'bxxxx;
             default: alu_control = 4'bxxxx;
         endcase
     end
