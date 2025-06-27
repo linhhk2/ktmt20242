dda9271bad650a3f32ce12bdb5b891f6d73d8bcf
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-06-27 22:16:57 +0700

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,27 +1,30 @@
-// control_unit.v
+// --- File: control_unit.v ---
+`include "defines.v"
+
 module control_unit(
     input  [6:0] opcode,
     input  [2:0] funct3,
     input  [6:0] funct7,
-    // Outputs from main_decoder
     output       RegWEn,
     output       ALUSrc,
     output       MemRW,
     output       MemToReg,
     output       Branch,
+    output       BrUn,
     output       Jump,
-    // Output from ALU_decoder
     output [3:0] alu_control
 );
     wire [1:0] alu_op;
 
     main_decoder md(
         .opcode(opcode),
+        .funct3(funct3),
         .RegWEn(RegWEn),
         .ALUSrc(ALUSrc),
         .MemRW(MemRW),
         .MemToReg(MemToReg),
         .Branch(Branch),
+        .BrUn(BrUn),
         .Jump(Jump),
         .ALUOp(alu_op)
     );
