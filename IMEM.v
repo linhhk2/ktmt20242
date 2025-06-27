e35d8ecdad26918398055d9592eb2bea83fb22b1
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-06-27 22:19:07 +0700

    Update IMEM.v

--- IMEM.v
+++ IMEM.v
@@ -1,23 +1,15 @@
-// IMEM.v
-// Mô phỏng bộ nhớ lệnh (Instruction Memory).
-
+// --- File: IMEM.v ---
 module IMEM(
-    input  [31:0] address,       // Địa chỉ của lệnh cần nạp (từ PC)
-    output reg [31:0] instruction  // Lệnh được đọc ra
+    input  [31:0] address,
+    output reg [31:0] instruction
 );
-    // Khai báo bộ nhớ lệnh, cũng có kích thước 4KB
     reg [31:0] memory [0:1023];
 
-    // Khối initial được thực thi một lần khi bắt đầu mô phỏng
-    // để nạp mã máy từ file program.hex vào bộ nhớ.
     initial begin
         $readmemh("program.hex", memory);
     end
 
-    // Logic đọc lệnh (tổ hợp/bất đồng bộ)
-    // Luôn xuất ra lệnh tại địa chỉ mà PC trỏ tới.
     always @(*) begin
         instruction = memory[address[11:2]];
     end
-
 endmodule
