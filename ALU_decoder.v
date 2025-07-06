5fe3a726f5be9db33a3987d95fd3e6dfcf7cc69f
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-07-06 22:34:33 +0700

    Update ALU_decoder.v

--- ALU_decoder.v
+++ ALU_decoder.v
@@ -3,16 +3,16 @@
 module ALU_decoder(
     input  [1:0] ALUOp,
     input  [2:0] funct3,
-    input  [6:0] funct7, 
+    input  [6:0] funct7,
     output reg [3:0] alu_control
 );
 
-    // Dây nội bộ để trích xuất bit quan trọng
     wire funct7b5 = funct7[5];
 
     always @(*) begin
         case(ALUOp)
-            2'b00: // R-Type
+            // ALUOp '00' dành riêng cho các lệnh R-type
+            2'b00:
                 case(funct3)
                     `FUNCT3_ADD_SUB: alu_control = (funct7b5) ? `ALU_SUB : `ALU_ADD;
                     `FUNCT3_SLL:     alu_control = `ALU_SLL;
@@ -24,9 +24,14 @@ module ALU_decoder(
                     `FUNCT3_AND:     alu_control = `ALU_AND;
                     default:         alu_control = 4'hX;
                 endcase
-            2'b01: 
+
+            // ALUOp '01' CHỈ CÓ NGHĨA LÀ PHÉP CỘNG
+            // Dùng cho LOAD, STORE, LUI, AUIPC, JAL, JALR
+            2'b01:
                 alu_control = `ALU_ADD;
-            2'b10: // I-Type
+
+            // ALUOp '10' dành riêng cho các lệnh I-type số học
+            2'b10:
                 case(funct3)
                     `FUNCT3_ADDI:    alu_control = `ALU_ADD;
                     `FUNCT3_SLTI:    alu_control = `ALU_SLT;
@@ -35,11 +40,15 @@ module ALU_decoder(
                     `FUNCT3_ORI:     alu_control = `ALU_OR;
                     `FUNCT3_ANDI:    alu_control = `ALU_AND;
                     `FUNCT3_SLLI:    alu_control = `ALU_SLL;
-                    `FUNCT3_SRLI_SRAI: alu_control = (funct7[5]) ? `ALU_SRA : `ALU_SRL; 
+                    `FUNCT3_SRLI_SRAI: alu_control = (funct7b5) ? `ALU_SRA : `ALU_SRL;
                     default:         alu_control = 4'hX;
                 endcase
-            2'b11: // Cho branches
+
+            // ALUOp '11' CHỈ CÓ NGHĨA LÀ PHÉP TRỪ
+            // Dùng riêng cho BRANCH để so sánh
+            2'b11:
                 alu_control = `ALU_SUB;
+                
             default: alu_control = 4'hX;
         endcase
     end
