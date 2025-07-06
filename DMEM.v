f063230dc356b2ce108180998b28eb49bf3f63a0
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-07-06 23:15:25 +0700

    Update DMEM.v

--- DMEM.v
+++ DMEM.v
@@ -8,17 +8,13 @@ module DMEM(
 );
     reg [31:0] memory [0:1023];
 
-    // zero‑init bộ nhớ dữ liệu
-    integer i;
-    initial begin
-        for (i = 0; i < 1024; i = i + 1)
-            memory[i] = 32'b0;
+    always @(*) begin
+        read_data = memory[address[11:2]];
     end
 
-    // đọc bất đồng bộ
-    always @(*) read_data = memory[address[11:2]];
-
-    // ghi đồng bộ
-    always @(posedge clk)
-        if (MemRW) memory[address[11:2]] <= write_data;
+    always @(posedge clk) begin
+        if (MemRW) begin
+            memory[address[11:2]] <= write_data;
+        end
+    end
 endmodule
