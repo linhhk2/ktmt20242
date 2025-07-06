52fc8fb4a042b439ccc946af8b4d8a581f846a04
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-07-06 22:59:14 +0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,22 +1,18 @@
-// --- File: RegisterFile.v ---
 module RegisterFile(
-    input         clk,
-    input         RegWEn,
-    input  [4:0]  rs1_addr,
-    input  [4:0]  rs2_addr,
-    input  [4:0]  rd_addr,
-    input  [31:0] rd_data,
-    output [31:0] rs1_data,
-    output [31:0] rs2_data
+    input              clk,
+    input              RegWEn,
+    input      [4:0]   rs1_addr, rs2_addr, rd_addr,
+    input      [31:0]  rd_data,
+    output reg [31:0]  rs1_data, rs2_data
 );
-    reg [31:0] registers [31:0];
+    reg [31:0] regs [0:31];
 
-    assign rs1_data = (rs1_addr == 5'b0) ? 32'd0 : registers[rs1_addr];
-    assign rs2_data = (rs2_addr == 5'b0) ? 32'd0 : registers[rs2_addr];
-
-    always @(posedge clk) begin
-        if (RegWEn && (rd_addr != 5'b0)) begin
-            registers[rd_addr] <= rd_data;
-        end
+    always @(*) begin
+        rs1_data = (rs1_addr == 5'd0) ? 32'b0 : regs[rs1_addr];
+        rs2_data = (rs2_addr == 5'd0) ? 32'b0 : regs[rs2_addr];
     end
+
+    always @(posedge clk)
+        if (RegWEn && rd_addr != 5'd0)
+            regs[rd_addr] <= rd_data;
 endmodule
