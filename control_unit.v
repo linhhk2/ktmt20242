1667e33ac9d6e28c6b434aafdb2dfb554d60095b
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-07-06 17:41:34 +0700

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,4 +1,3 @@
-// --- File: control_unit.v ---
 `include "defines.v"
 
 module control_unit(
@@ -30,7 +29,7 @@ module control_unit(
     );
 
     ALU_decoder ad(
-        .funct7(funct7),
+        .funct7(funct7), 
         .funct3(funct3),
         .ALUOp(alu_op),
         .alu_control(alu_control)
