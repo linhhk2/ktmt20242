cdb505be135ac94cf873fa7d7c38e3b8c49785b3
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-07-06 22:33:30 +0700

    Update Branch_Comp.v

--- Branch_Comp.v
+++ Branch_Comp.v
@@ -1,21 +1,24 @@
-// --- File: Branch_Comp.v ---
 `include "defines.v"
 
 module Branch_Comp(
     input [31:0] A,
     input [31:0] B,
-    input        BrUn,
+    input        BrUn, 
     input [2:0]  funct3,
     output reg   BranchTaken
 );
+
+    wire slt = BrUn ? (A < B) : ($signed(A) < $signed(B));
+    wire eq  = (A == B);
+
     always @(*) begin
         case (funct3)
-            `FUNCT3_BEQ:  BranchTaken = (A == B);
-            `FUNCT3_BNE:  BranchTaken = (A != B);
-            `FUNCT3_BLT:  BranchTaken = ($signed(A) < $signed(B));
-            `FUNCT3_BGE:  BranchTaken = ($signed(A) >= $signed(B));
-            `FUNCT3_BLTU: BranchTaken = (A < B);
-            `FUNCT3_BGEU: BranchTaken = (A >= B);
+            `FUNCT3_BEQ:  BranchTaken = eq;
+            `FUNCT3_BNE:  BranchTaken = !eq;
+            `FUNCT3_BLT:  BranchTaken = slt;
+            `FUNCT3_BGE:  BranchTaken = !slt;
+            `FUNCT3_BLTU: BranchTaken = slt;
+            `FUNCT3_BGEU: BranchTaken = !slt;
             default:      BranchTaken = 1'b0;
         endcase
     end
