ad595a8ec1308eeb0b45f7950ea06787c79bd608
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-07-06 23:08:18 +0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,3 +1,4 @@
+// --- File: RegisterFile.v ---
 module RegisterFile(
     input              clk,
     input              RegWEn,
@@ -6,6 +7,11 @@ module RegisterFile(
     output reg [31:0]  rs1_data, rs2_data
 );
     reg [31:0] registers [0:31];
+    integer i;
+    initial begin
+        for (i = 0; i < 32; i = i + 1)
+            registers[i] = 32'b0;
+    end
 
     always @(*) begin
         rs1_data = (rs1_addr == 5'd0) ? 32'b0 : registers[rs1_addr];
