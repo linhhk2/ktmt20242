6449cb062cb4e9587f108cf930886c0b0f068309
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-07-06 22:32:23 +0700

    Update main_decoder.v

--- main_decoder.v
+++ main_decoder.v
@@ -21,43 +21,42 @@ module main_decoder(
         case (opcode)
             `OPCODE_R: begin
                 RegWEn = 1'b1;
-                ALUOp = 2'b00;
+                ALUOp = 2'b00; // Dùng '00' cho R-Type
             end
             `OPCODE_I_ARITH: begin
                 RegWEn = 1'b1; ALUSrc = 1'b1;
-                ALUOp = 2'b10;
+                ALUOp = 2'b10; // Dùng '10' cho I-Type
             end
             `OPCODE_LOAD: begin
                 RegWEn = 1'b1; ALUSrc = 1'b1; MemToReg = 1'b1;
-                ALUOp = 2'b01;
+                ALUOp = 2'b01; // Dùng '01' cho tính toán địa chỉ
             end
             `OPCODE_S: begin
                 ALUSrc = 1'b1; MemRW = 1'b1;
-                ALUOp = 2'b01;
+                ALUOp = 2'b01; // Dùng '01' cho tính toán địa chỉ
             end
             `OPCODE_B: begin
                 Branch = 1'b1;
-                ALUOp = 2'b11;
+                ALUOp = 2'b11; // Dùng '11' riêng cho Branch
                 if (funct3 == `FUNCT3_BLTU || funct3 == `FUNCT3_BGEU) begin
                     BrUn = 1'b1;
                 end
             end
             `OPCODE_LUI: begin
                 RegWEn = 1'b1; ALUSrc = 1'b1;
-                ALUOp = 2'b01; 
+                ALUOp = 2'b01; // Dùng '01' để ALU luôn thực hiện phép ADD
             end
             `OPCODE_AUIPC: begin
                 RegWEn = 1'b1; ALUSrc = 1'b1;
-                ALUOp = 2'b01;
+                ALUOp = 2'b01; // Dùng '01' để ALU luôn thực hiện phép ADD
             end
             `OPCODE_JAL: begin
-                RegWEn = 1'b1; Jump = 1'b1;
-                ALUSrc = 1'b1; 
-                ALUOp = 2'b01;
+                RegWEn = 1'b1; Jump = 1'b1; ALUSrc = 1'b1;
+                ALUOp = 2'b01; // Dùng '01' để ALU luôn thực hiện phép ADD
             end
             `OPCODE_JALR: begin
                 RegWEn = 1'b1; Jump = 1'b1; ALUSrc = 1'b1;
-                ALUOp = 2'b01;
+                ALUOp = 2'b01; // Dùng '01' để ALU luôn thực hiện phép ADD
             end
         endcase
     end
